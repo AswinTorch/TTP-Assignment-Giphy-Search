89e525105002146c587e8ca98e592aa004d77ec2
Andrea Habib <andrea.atef@yahoo.com>
2020-06-04 15:27:36 -0400

    worked on search

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class SearchField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: "",
+    };
+  }
+  search = (e) => {
+    this.setState({ searchInput: e.target.value });
+  };
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <input type="text" onChange={this.search}></input>
+        <button onSubmit={this.submit}>Submit</button>
+      </div>
+    );
   }
 }
 
