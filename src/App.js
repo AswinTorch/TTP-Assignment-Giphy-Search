0a84e21a5885a643a94e90b479ccae899025dc9f
Aswin Nair <aswintorch@gmail.com>
2020-06-04 14:40:11 -0400

    Added a data fetch when page loads to grab trending gifs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import GIFCard from "./components/GIFCard";
 import SearchField from "./components/SearchField";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
@@ -9,6 +10,27 @@ class App extends Component {
     this.state = {
       results: [],
     };
+  }
+
+  // On app load
+  componentDidMount() {
+    const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
+    // Fetches trending gifs
+    axios
+      .get(`http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=9`)
+      .then((response) => {
+        // Handles success
+        const results = response.data.data;
+        console.log(results);
+        // Sets data in state
+        this.setState({
+          results,
+        });
+      })
+      .catch((error) => {
+        // Handles error
+        console.log(error);
+      });
   }
 
   render() {
